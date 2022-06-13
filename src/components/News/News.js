980d4d87c9a5e3a373ccef2b99c22e69c4c2baf0
mohd-alfaiz-khan <captainalfaiz@gmail.com>
2022-06-13 15:37:32 +0530

    add sample json and static card UI

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,12 +1,35 @@
 import * as React from "react";
 
 import { NewsItem } from "../../components";
+import SampleJson from "../../sampleOutput.json";
 
 class News extends React.Component {
+  articles = SampleJson.articles;
+  constructor() {
+    super();
+    this.state = {
+      articles: this.articles,
+      loading: false,
+    };
+  }
   render() {
     return (
-      <div className="news">
-        News Component <NewsItem />
+      <div className="container my-3 news">
+        <h2>Top Headlines!</h2>
+        <div className="row">
+          {this.state.articles.map((article) => {
+            return (
+              <div className="col-md-4" key={article.url}>
+                <NewsItem
+                  title={article.title}
+                  description={article.description}
+                  imageUrl={article.urlToImage}
+                  newsUrl={article.url}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
--- src/components/NewsItem/NewsItem.js
+++ src/components/NewsItem/NewsItem.js
@@ -2,7 +2,21 @@ import * as React from "react";
 
 class NewsItem extends React.Component {
   render() {
-    return <div className="news-item">NewsItem Component</div>;
+    const { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div className="news-item my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imageUrl} className="card-img-top" alt={title} />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-sm btn-dark">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
